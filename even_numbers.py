c7bc0dcd9d8b67f22d53ef1fab27298a35983b96
mansanael <magathnael@meltwater.org>
2022-10-23 11:31:11 +0000

    Even and Odd done

---
+++ even_numbers.py
@@ -0,0 +1,10 @@
+for i in range(2, 31,2):
+    print(i)
+
+even = 1
+while even < 31 :
+    if (even%2 == 0):
+        print(even)
+    even += 1
+
+
---
+++ odd_numbers.py
@@ -0,0 +1,10 @@
+for i in range(1, 31,2):
+    print(i)
+
+odd = 1
+while odd < 31 :
+    if (odd%2 != 0):
+        print(odd)
+    odd += 1
+
+
